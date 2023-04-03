e77c0e7c7a5a56191a942c672dea375ff07c70e0
Nasif Shahriar Mohim <53572742+shahriarnasif@users.noreply.github.com>
2023-04-04 02:13:39 +0600

    django

--- Day_17/django/store/urls.py
+++ Day_17/django/store/urls.py
@@ -6,5 +6,6 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.all_products, name='all_products'),
+    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
    
 ]
--- Day_17/django/store/views.py
+++ Day_17/django/store/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import Category, Product
 
+from django.shortcuts import get_object_or_404,render
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
 
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/detail.html', {'product': product})
